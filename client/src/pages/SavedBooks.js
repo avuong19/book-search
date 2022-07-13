fb94f319703acd21df97a7d89d54c3c25e30f287
Anh Vuong <vta19797@gmail.com>
2022-07-13 01:29:10 -0500

    deploying

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
-
 import { useQuery, useMutation } from '@apollo/client';
 import { GET_ME } from '../utils/queries';
 import { REMOVE_BOOK } from '../utils/mutations';
-
 
 const SavedBooks = () => {
   const { loading, data } = useQuery(GET_ME);
@@ -33,7 +30,6 @@ const SavedBooks = () => {
     }
   };
 
-  // if data isn't here yet, say so
   if (loading) {
     return <h2>LOADING...</h2>;
   }
